ea3ae7813116515c8dc425e397cdef9927c109db
CSynodinos <87645293+CSynodinos@users.noreply.github.com>
2021-07-19 17:54:30 +0100

    Update Item_Builder.py

--- Item_Builder.py
+++ Item_Builder.py
@@ -23,9 +23,7 @@ def Load_Champs():
     return Champ_Base_Stats
 
 
-### Function that uses the inputs and assigns them as variables.
-### By assignin the inputs as globals, they can be used by the Champ_Stats class.
-def myInputs():
+def myInputs(): # Function that uses the inputs and assigns them as variables.
     global inputChampion
     global lvl
     global inputItem1
@@ -43,7 +41,6 @@ def myInputs():
     inputItem5 = str(e6.get())
     inputItem6 = str(e7.get())
 
-    ### if/else statement that checks if inputs have been inserted or not.
     if inputChampion:
         root.destroy()
         pass
@@ -51,7 +48,6 @@ def myInputs():
         root.destroy()
         tkinter.messagebox.showerror("Error", "Please select a Champion!")
 
-## Pop-up window if the close button (X) is pressed.
 def on_closing():
     if tkinter.messagebox.askokcancel("Exit", "Do you want to quit?"):
         root.destroy()
@@ -71,8 +67,6 @@ def item_select(input_item, column):
     return i
 
 def builder():
-    # All the variables needed for the calculations in the main builder script.
-    # Champion Base Stat Variables
     HP = base(inputChampion, 1)
     HPg = base(inputChampion,2)
     MP = base(inputChampion,3)
@@ -87,9 +81,7 @@ def builder():
     MRg = base(inputChampion,12)
     MS = base(inputChampion,13)
 
-    # Item Stat Variables. iloc is location based indexing function. int converts location values into integers for calculations.
-
-    ## Item 1. 
+    # Item 1. 
     Item_1_AD = item_select(inputItem1, 1)
     Item_1_AP = item_select(inputItem1, 2)
     Item_1_AS = item_select(inputItem1, 3)
@@ -102,7 +94,7 @@ def builder():
     Item_1_Haste = item_select(inputItem1, 10)
     Item_1_MS = item_select(inputItem1, 11)
 
-    ## Item 2.
+    # Item 2.
     Item_2_AD = item_select(inputItem2, 1)
     Item_2_AP = item_select(inputItem2, 2)
     Item_2_AS = item_select(inputItem2, 3)
@@ -115,7 +107,7 @@ def builder():
     Item_2_Haste = item_select(inputItem2, 10)
     Item_2_MS = item_select(inputItem2, 11)
 
-    ## Item 3.
+    # Item 3.
     Item_3_AD = item_select(inputItem3, 1)
     Item_3_AP = item_select(inputItem3, 2)
     Item_3_AS = item_select(inputItem3, 3)
@@ -128,7 +120,7 @@ def builder():
     Item_3_Haste = item_select(inputItem3, 10)
     Item_3_MS = item_select(inputItem3, 11)
 
-    ## Item 4.
+    # Item 4.
     Item_4_AD = item_select(inputItem4, 1)
     Item_4_AP = item_select(inputItem4, 2)
     Item_4_AS = item_select(inputItem4, 3)
@@ -141,7 +133,7 @@ def builder():
     Item_4_Haste = item_select(inputItem4, 10)
     Item_4_MS = item_select(inputItem4, 11)
 
-    ## Item 5.
+    # Item 5.
     Item_5_AD = item_select(inputItem5, 1)
     Item_5_AP = item_select(inputItem5, 2)
     Item_5_AS = item_select(inputItem5, 3)
@@ -154,7 +146,7 @@ def builder():
     Item_5_Haste = item_select(inputItem5, 10)
     Item_5_MS = item_select(inputItem5, 11)
 
-    ## Item 6.
+    # Item 6.
     Item_6_AD = item_select(inputItem6, 1)
     Item_6_AP = item_select(inputItem6, 2)
     Item_6_AS = item_select(inputItem6, 3)
@@ -169,27 +161,26 @@ def builder():
 
     # Base Stat Calculations.
 
-    ## Health.
+    # Health.
     Health = HP + HPg*(lvl-1)*(0.7025+0.0175*(lvl-1)) + (Item_1_HP + Item_2_HP + Item_3_HP + Item_4_HP + Item_5_HP + Item_6_HP)
 
-    ## Mana.
+    # Mana.
     Mana = MP + MPg*(lvl-1)*(0.7025+0.0175*(lvl-1)) + (Item_1_MP + Item_2_MP + Item_3_MP + Item_4_MP + Item_5_MP + Item_6_MP)
 
-    ## Attack Speed.
+    # Attack Speed.
     Attack_Speed_Base = AS + ASg*(lvl-1)*(0.7025+0.0175*(lvl-1))
     Total_Item_AS = Item_1_AS + Item_2_AS + Item_3_AS + Item_4_AS + Item_5_AS + Item_6_AS
 
-    ### if that checks for Jhin.
+    # if that checks for Jhin.
     if inputChampion in ('Jhin'):
         Attack_Speed = Attack_Speed_Base
-    ### if Attack speed is >2.5, it normalizes at 2.5.
     elif Total_Item_AS >= 210:
         Attack_Speed = 2.5
     elif Total_Item_AS <= 210:
         Attack_Speedt = Attack_Speed_Base * (1 + (Total_Item_AS/100))
         Attack_Speed = float(Attack_Speedt)
 
-    ### Temp Critical and Life Steal Variables.
+    # Temp Critical and Life Steal Variables.
     Critical_Striket = Item_1_Critical + Item_2_Critical + Item_3_Critical + Item_4_Critical + Item_5_Critical + Item_6_Critical    
     Life_Stealt = Item_1_Life_Steal + Item_2_Life_Steal + Item_3_Life_Steal + Item_4_Life_Steal + Item_5_Life_Steal + Item_6_Life_Steal
 
@@ -198,7 +189,6 @@ def builder():
 
     ### Armor Penetration.
 
-    ### If statement checks if critical and life steal exceed 100%.
     if Critical_Striket > 100 or Life_Stealt > 100:
         Critical_Strike = 100
         Life_Steal = 100
@@ -206,15 +196,12 @@ def builder():
         Critical_Strike = Critical_Striket
         Life_Steal = Life_Stealt
 
-    ## Attack Damage.
-    ### if checks if the selected champion is Jhin.
-    if inputChampion in ('Jhin'):
-        ### Function for the counter added inthe ad bonus Jhin gets from lvl ups.      
+    # Attack Damage.
+    # if checks if the selected champion is Jhin.
+    if inputChampion in ('Jhin'):     
         def Counter(counter_1):
             counter_1 = counter_1 + lvl
-            return counter_1
-
-        ### if sets the bonus lvl ad % according to the input lvl.      
+            return counter_1      
         if 1 <= lvl <= 9: 
             counter_1 = 0
             for i in range(1):
@@ -273,41 +260,37 @@ def builder():
                 counter_1 = Counter(counter_1)
                 bonus_lvl_AD = extra + counter_1
         
-        ### AD calculations for Jhin.
+        # AD calculations for Jhin.
         Attack_DMG_base = AD + ADg*(lvl-1)*(0.7025+0.0175*(lvl-1))
         Attack_DMG_base_item = Attack_DMG_base + (Item_1_AD + Item_2_AD + Item_3_AD + Item_4_AD + Item_5_AD + Item_6_AD)         
         Attack_DMG_base_item_crit_as = Attack_DMG_base_item + ((Critical_Strike * 0.3) + (Total_Item_AS * 0.25))
         Attack_DMG_lvl = Attack_DMG_base_item_crit_as * (bonus_lvl_AD/100)
         Attack_DMG = Attack_DMG_base_item_crit_as + Attack_DMG_lvl
     else:
-        ### AD calculations for all other Champs.
+        # AD calculations for all other Champs.
         Attack_DMG = AD + ADg*(lvl-1)*(0.7025+0.0175*(lvl-1)) + (Item_1_AD + Item_2_AD + Item_3_AD + Item_4_AD + Item_5_AD + Item_6_AD)
 
-    ## Ability Power.
+    # Ability Power.
     Ability_Power = Item_1_AP + Item_2_AP + Item_3_AP + Item_4_AP + Item_5_AP + Item_6_AP
 
-    ## Champion Armor.
+    # Champion Armor.
     Armor = AR + ARg*(lvl-1)*(0.7025+0.0175*(lvl-1)) + (Item_1_AR + Item_2_AR + Item_3_AR + Item_4_AR + Item_5_AR + Item_6_AR)
 
-    ## Champion Magic Resistance.
+    # Champion Magic Resistance.
     Magic_R = MR + MRg*(lvl-1)*(0.7025+0.0175*(lvl-1)) + (Item_1_MR + Item_2_MR + Item_3_MR + Item_4_MR + Item_5_MR + Item_6_MR)
 
-    ## Ability Haste.
+    # Ability Haste.
     Haste = Item_1_Haste + Item_2_Haste + Item_3_Haste + Item_4_Haste + Item_5_Haste + Item_6_Haste
 
-    ## Movement Speed.
+    # Movement Speed.
     Total_Item_Movement_Speed = Item_1_MS + Item_2_MS + Item_3_MS + Item_4_MS + Item_5_MS + Item_6_MS
     Movement_Speed = MS * (1 + (Total_Item_Movement_Speed/100))
-
-    # Create a workbook and add a worksheet.
-    ## Output filename contains Champ name as a string.
+    
     Output_filename = str(inputChampion) + "_Build" + ".xlsx"
-
-    ## Opens an xlsx file to write the output results.
+    
     workbook = xlsxwriter.Workbook(Output_filename)
     worksheet = workbook.add_worksheet()
 
-    ## Structure of output file.
     results = (
         ['Champion', inputChampion],
         ['Level', lvl],
@@ -330,17 +313,14 @@ def builder():
         ['Item 6', inputItem6],
     )
 
-    ## Starting rows and columns.
     row = 0
     col = 0
 
-    ## Loop adding the results.
     for Champ, stat in (results):
         worksheet.write(row, col, Champ)
         worksheet.write(row, col + 1, stat)
         row += 1
 
-    ## Close workbook and exit program.
     workbook.close()
     tkinter.messagebox.showinfo("Status", "Build Generated. Press OK to continue")
 
@@ -354,101 +334,82 @@ if __name__== "__main__":
     # Indexing of Champion Spreadsheet.
     df2 = pd.DataFrame(Load_Champs(), columns = ['Champion', 'HP', 'HPg', 'MP', 'MPg', 'AD', 'ADg', 'AS', 'ASg', 'AR', 'ARg', 'MR', 'MRg', 'MS'])
 
-    # The Input pop-up box for Champion, Level and Item selection.
-    ## Open an Input box with 8 inputs and label the first input.
     root = Tk()
     root.title("Item Builder")
 
-    ## Label Champion Input.
     labelText1=StringVar()
     labelText1.set("Champion")
     labelDir=Label(root, textvariable=labelText1, height=2)
     labelDir.pack()
 
-    ## Allow for the first input to be written. Input is a string variable.
     directory1=StringVar(None)
     e1=Entry(root,textvariable=directory1,width=50)
     e1.pack()
-
-    ## Label of Level Input.
+    
+    # Labels and Inputs
     labelTextlvl=StringVar()
     labelTextlvl.set("Level")
     labelDir=Label(root, textvariable=labelTextlvl, height=2)
     labelDir.pack()
-
-    ## Allow for the Level input to be written. Input is a string variable.
+    
     directorylvl=StringVar(None)
     e0=Entry(root,textvariable=directorylvl,width=50)
     e0.pack()
 
-    ## Label Item 1 Input.
     labelText2=StringVar()
     labelText2.set("Item 1")
     labelDir=Label(root, textvariable=labelText2, height=2)
     labelDir.pack()
-
-    ## Allow for Item 1 input to be written. Input is a string variable.
+    
     directory2=StringVar(None)
     e2=Entry(root,textvariable=directory2,width=50)
     e2.pack()
 
-    ## Label the Item 2 Input.
     labelText3=StringVar()
     labelText3.set("Item 2")
     labelDir=Label(root, textvariable=labelText3, height=2)
     labelDir.pack()
 
-    ## Allow for Item 2 input to be written. Input is a string variable.
     directory3=StringVar(None)
     e3=Entry(root,textvariable=directory3,width=50)
     e3.pack()
 
-
-    ## Label the Item 3 Input.
     labelText4=StringVar()
     labelText4.set("Item 3")
     labelDir=Label(root, textvariable=labelText4, height=2)
     labelDir.pack()
-
-    ## Allow for Item 3 input to be written. Input is a string variable.
+    
     directory4=StringVar(None)
     e4=Entry(root,textvariable=directory4,width=50)
     e4.pack()
-
-    ## Label the Item 4 Input.
+    
     labelText5=StringVar()
     labelText5.set("Item 4")
     labelDir=Label(root, textvariable=labelText5, height=2)
     labelDir.pack()
 
-    ## Allow for Item 4 input to be written. Input is a string variable.
     directory5=StringVar(None)
     e5=Entry(root,textvariable=directory5,width=50)
     e5.pack()
 
-    ## Label the Item 5 Input.
     labelText6=StringVar()
     labelText6.set("Item 5")
     labelDir=Label(root, textvariable=labelText6, height=2)
     labelDir.pack()
 
-    ## Allow for Item 5 input to be written. Input is a string variable.
     directory6=StringVar(None)
     e6=Entry(root,textvariable=directory6,width=50)
     e6.pack()
 
-    ## Label the Item 6 Input.
     labelText7=StringVar()
     labelText7.set("Item 6")
     labelDir=Label(root, textvariable=labelText7, height=2)
     labelDir.pack()
 
-    ## Allow for Item 6 input to be written. Input is a string variable.
     directory7=StringVar(None)
     e7=Entry(root,textvariable=directory7,width=50)
     e7.pack()
     
-    ## The button to run the code.
     myButton = Button(root, text = "Build", command = lambda: myInputs())
     myButton.pack()
     
